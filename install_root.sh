2bbbe47ec7fa19e2f79b84c82d414a621b89f962
Mikhail Tikhomirov <tikhomirov@reshenie-soft.ru>
2022-08-31 13:01:41 +0300

    Bad path to script

--- install_root.sh
+++ install_root.sh
@@ -345,7 +345,7 @@ then
   	apt-get update && apt-get dist-upgrade -y
     if [[ $longbit -eq 0 ]];
     then
-        /bin/bash /alt_wine_install.sh
+        /bin/bash /home/$username/linux_installer/alt_wine_install.sh
         apt-get install wine-gecko wine-mono winetricks -y
     else
   	    apt-get install i586-wine.32bit wine-gecko wine-mono winetricks -y
